82ade849b0861cc05b081c9a8359ce82421ac75a
sonnees <sson12131415@gmail.com>
2024-05-06 09:42:38 +0700

    fix +

--- src/main/java/com/zalolite/gatewayservice/CorsConfig.java
+++ src/main/java/com/zalolite/gatewayservice/CorsConfig.java
@@ -1,8 +1,16 @@
 package com.zalolite.gatewayservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class CorsConfig implements WebFluxConfigurer {
+public class CorsConfig {
+    @Bean
+    @Primary
+    public CorsWebFilter corsWebFilter() {
+        return new CorsWebFilter(new UrlBasedCorsConfigurationSource());
+    }
 }

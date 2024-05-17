c304c2bfcee84d49b75baecd8715f724d9c6bd3f
sonnees <sson12131415@gmail.com>
2024-05-17 14:03:55 +0700

    change domain

--- src/main/java/com/zalolite/gatewayservice/CorsConfig.java
+++ src/main/java/com/zalolite/gatewayservice/CorsConfig.java
@@ -13,12 +13,17 @@ public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
+        String variableValue = System.getenv("frontend_url");
+
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedOrigin("http://localhost:3005");
         corsConfiguration.addAllowedOrigin("https://zalo-lite-deploy-sj33-a20eiw8cb-tranquanghuy-09s-projects.vercel.app");
+        corsConfiguration.addAllowedOrigin("https://www.zaloweb.click");
         corsConfiguration.addAllowedOrigin("http://localhost:5173");
         corsConfiguration.addAllowedOrigin("http://localhost:80");
+        corsConfiguration.addAllowedOrigin(variableValue);
         corsConfiguration.addAllowedMethod("GET");
         corsConfiguration.addAllowedMethod("POST");
         corsConfiguration.addAllowedHeader("Origin");
